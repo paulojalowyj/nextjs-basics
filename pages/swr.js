59d2b3be2b17fbfb1366825e837e0c166644dc8c
Paulo Jalowyj <paulo@agenciacapre.com.br>
2020-10-22 22:30:20 -0300

    Modify SWR to SSR

--- pages/swr.js
+++ pages/swr.js
@@ -1,9 +1,16 @@
 import useSWR from "swr";
 import Layout from "../components/Layout";
 
-function Profile() {
-  const { data, error } = useSWR("http://localhost:3000/api/user");
-
+export async function getStaticProps() {
+  // `getStaticProps` is invoked on the server-side,
+  // so this `fetcher` function will be executed on the server-side.
+  const posts = await fetcher("/api/user");
+  return { props: { name } };
+}
+function Props(props) {
+  // Here the `fetcher` function will be executed on the client-side.
+  const { data } = useSWR("/api/user", fetcher, { initialData: props.name });
+  // ...
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
   return (
@@ -12,5 +19,3 @@ function Profile() {
     </Layout>
   );
 }
-
-export default Profile;
