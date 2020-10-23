5e7b328024643fabbb930f5acb264f3740e03016
Paulo Jalowyj <paulo@agenciacapre.com.br>
2020-10-22 22:39:10 -0300

    Add Export Default

--- pages/swr.js
+++ pages/swr.js
@@ -1,18 +1,21 @@
 import useSWR from "swr";
 import Layout from "../components/Layout";
 
-export async function getStaticProps() {
-  // `getStaticProps` is invoked on the server-side,
-  // so this `fetcher` function will be executed on the server-side.
-  const posts = await fetcher("/api/user");
-  return { props: { name } };
-}
-function Profile(props) {
-  // Here the `fetcher` function will be executed on the client-side.
-  const { data } = useSWR("/api/user", fetcher, { initialData: props.name });
-  // ...
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+function Profile() {
+  const { data, error } = useSWR("/api/user");
+
+  if (error)
+    return (
+      <Layout>
+        <h1>failed to load</h1>
+      </Layout>
+    );
+  if (!data)
+    return (
+      <Layout>
+        <h1>loading...</h1>
+      </Layout>
+    );
   return (
     <Layout>
       <h1>hello {data.name}!</h1>
